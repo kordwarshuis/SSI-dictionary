c579d98c1f50f43cbabc52c54ff61a6926dca3f8
Kor Dwarshuis <proximus@fastmail.com>
2026-05-07 22:25:40 +0200

    style(DictionaryView): enhance layout of search bar and container for better responsiveness

--- components/DictionaryView.vue
+++ components/DictionaryView.vue
@@ -150,16 +150,18 @@ function safeHtml(html) {
 </script>
 
 <template>
-  <div class="glossaries-combined">
-    <!-- Search bar -->
-    <div class="input-group mb-3">
-      <span class="input-group-text" id="search-addon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
-          viewBox="0 0 16 16" aria-hidden="true">
-          <path
-            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
-      </span>
+  <div class="glossaries-combined container px-3">
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-10 col-xl-9 col-xxl-8 mx-auto">
+        <!-- Search bar -->
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="search-addon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+              viewBox="0 0 16 16" aria-hidden="true">
+              <path
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
+          </span>
       <input v-model="searchTerm" type="text" class="form-control" placeholder="Search terms…" aria-label="Search terms"
         aria-describedby="search-addon" />
     </div>
@@ -239,6 +241,8 @@ function safeHtml(html) {
         </ul>
       </li>
     </ul>
+      </div>
+    </div>
   </div>
 </template>
 
